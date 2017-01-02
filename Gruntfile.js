81e4d12ac16b56cdc4d16d76fb499880640e7f90
root <root@root.com>
2017-01-02 20:36:52 +0100

    add grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,29 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+
+
+    require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         watch: {
             options: {
-                livereload: true,
+                livereload: true
             },
             css: {
-                files: ['css/**/*.css'],
+                files: ['css/**/*.css', 'dist/css/**/*.css']
             },
             js: {
-                files: ['js/**/*.js'],
+                files: ['js/**/*.js']
             },
             html: {
-                files: ['*.html'],
+                files: ['*.html']
             },
             jpg: {
-                files: ['images/**/*.jpg'],
+                files: ['images/**/*.jpg']
+            },
+            grunt: {
+                files: ['Gruntfile.js']
             }
         },
         connect: {
@@ -28,16 +34,103 @@ module.exports = function(grunt) {
                     hostname: '0.0.0.0',
                     protocol: 'http',
                     livereload: true,
-                    open: true,
+                    open: true
+                }
+            },
+            serve: {
+                options: {
+                    port: 9001,
+                    base: './dist',
+                    hostname: '0.0.0.0',
+                    protocol: 'http',
+                    livereload: true,
+                    open: true
                 }
             }
         },
+        cssmin: {
+            generated: {
+                files: [{
+                    dest: 'dist/css/style.min.css',
+                    src: ['.tmp/concat/assets/css/style.min.css']
+                }]
+            }
+        },
+        clean: {
+            dist: ['dist'],
+            tmp: ['.tmp']
+        },
+        concat: {
+            generated: {
+                files: [
+                    {
+                        dest: '.tmp/concat/assets/js/vendor.js',
+                        src: ['js/*.js']
+                    }, {
+                        dest: '.tmp/concat/assets/css/style.min.css',
+                        src: ['css/*.css']
+                    }
+                ]
+            }
+        },
+        concat_css: {
+            options: {
+                // Task-specific options go here.
+            },
+            all: {
+                src: ["build/css/*.min.css"],
+                dest: "release/css/styles.css"
+            }
+        },
+        uglify: {
+            generated: {
+                files: [
+                    {
+                        dest: 'dist/assets/js/vender.js',
+                        src: ['.tmp/concat/assets/js/vender.js']
+                    }
+                ]
+            }
+        },
+        usemin: {
+            html: 'dist/index.html'
+        },
+        useminPrepare: {
+            html: 'index.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+        copy: {
+            html: {
+                src: './index.html', dest: 'dist/index.html'
+            },
+            // css: {
+            //     src: './css/*', dest: 'dist/'
+            // },
+            img: {
+                src: './images/**', dest: 'dist/'
+            },
+            font: {
+                src: './fonts/**', dest: 'dist/'
+            }
+        }
     });
 
+    grunt.registerTask('serve', "Serve your app", [
+        'connect:serve', 'watch']);
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-
-    grunt.registerTask('server', ['connect','watch']);
+    grunt.registerTask('server', ['connect:server', 'watch']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy:html',
+        'copy:img',
+        'copy:font',
+        'useminPrepare',
+        'concat:generated',
+        'cssmin:generated',
+        'uglify:generated',
+        'usemin'
+    ]);
 
 };
